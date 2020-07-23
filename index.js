dfa7572eb78f142ba4944daf53ad78dae4c1fad8
JGeiser9 <32041734+JGeiser9@users.noreply.github.com>
2020-07-23 18:22:10 -0500

    add moment formatting and cleanup

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
-const axios = require('axios');
-const cron = require('node-cron');
-const Twit = require('twit');
+var axios = require('axios');
+var cron = require('node-cron');
+var Twit = require('twit');
+var moment = require('moment-timezone');
 
 // Twitter credentials
 const T = new Twit({
@@ -11,44 +12,42 @@ const T = new Twit({
   access_token_secret: process.env.AUTH_TOKEN_SECRET
 });
 
-// Send the post to twitter
-function post_status(status) {
-  T.post('statuses/update', { status: status }, (err, data) => {
-    err ? console.log(err) : console.log('Success: ' + data.text);
-  });
+// Send the posts to twitter
+function post_initial_status(incidents, current_date) {
+  const date = moment.unix(current_date).format("MM-DD-YYYY");
+
+  // Check for theft reports
+  if (incidents.length) {
+    incidents.map(bike => {
+      const theft_date = moment.unix(bike.occurred_at).format("MM-DD-YYYY");
+      let status = `${bike.title} | Stolen on ${theft_date} | ${bike.address} ${bike.source.html_url}`;
+
+      T.post('statuses/update', { status: status }, (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    });
+  } else {
+    console.log(`No reports in the past 24 hours | ${date}`);
+  }
 }
 
-// Build out a tweet for each theft report
-function build_status(data) {
-  data.map(bike => {
-    let status = `
-      Bike: ${bike.title}
-      Info: ${bike.description}
-      Where: ${bike.address}
-      Date: ${bike.occurred_at}
-    `;
-
-    post_status(status);
-  });
-};
-
 // Hit API for theft data
-async function get_data(url) {
+async function get_data(url, current_date) {
   try {
     const response = await axios.get(url);
-    build_status(response.data.incidents);
+    post_initial_status(response.data.incidents, current_date);
   } catch (error) {
     console.error(error);
   }
 }
 
-// Run theft report every Thursday at 12:00 PM
-cron.schedule('46 11 * * *', () => {
-  // Variable for current unix timestamp when job is run
-  // Variable of converted timestamp to America/Chicago timezone?
-  // Variable for a timestamp from one week ago
-  // Build out the API URL
-  const url = "https://bikewise.org:443/api/v2/incidents?page=1&occurred_after=1594857600&incident_type=theft&proximity=Minneapolis&proximity_square=100";
+// Run theft report at 12 every day for previous 24 hours
+cron.schedule('* 12 * * *', () => {
+  const yesterday_unix = moment().tz("America/Chicago").subtract(1, "days").unix();
+  const today_unix = moment().tz("America/Chicago").subtract(1, "minute").unix();
+  const url = `https://bikewise.org:443/api/v2/incidents?page=1&occurred_before=${today_unix}&occurred_after=${yesterday_unix}&incident_type=theft&proximity=Minneapolis&proximity_square=100`;
 
-  get_data(url);
+  get_data(url, today_unix);
 });
